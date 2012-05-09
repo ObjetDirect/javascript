9bf44dc85a0d35f00999feea7e9dc08798bf505b
Julien Roche <jroche@objetdirect.com>
2012-05-09 21:08:54 +0200

    First commit

---
+++ jQuery/globalization/README.md
@@ -0,0 +1,51 @@
+﻿# jQuery plugin for the globalization #
+
+This little plugin can load some globalization properties and translate our Web pages. We put on the DOM element the attribute "data-translate".
+This attribute can contains one or many properties, and inject these into the content, or into the attribute.
+
+Examples:
+<pre>
+  <!-- Inject into the content -->
+  <div data-translate="aProperty">A default text</div>
+</pre>
+
+<pre>
+  <!-- Inject into the attribute -->
+  <div data-translate="title:aProperty" title="A default title">A text</div>
+</pre>
+
+<pre>
+  <!-- Inject into the attribute and the content -->
+  <div data-translate="title:aProperty;anotherProperty" title="A default title">A default text</div>
+</pre>
+
+You can too retrieve the property manually:
+<pre>
+  var text = $.translate.properties["aProperty"];
+</pre>
+
+---
+
+### The MIT License
+
+Copyright (c) <2012> <Objet Direct>
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ 
+ 
---
+++ jQuery/globalization/jquery.globalization.js
@@ -0,0 +1,79 @@
+﻿/**
+ * Plugin to put some globalizations
+ */
+windows.jQuery && (function($){
+	if($.translate){
+		// Already loaded ?
+		return;
+	}
+
+	/**
+	 * Options
+	 */
+	$.translate = {
+		defaultLanguage: navigator.language,
+		pathLanguage: function(language){ return language + ".js" },
+		properties: {}
+	});
+	
+	/**
+	 * Override the HTML method to translate when we need
+	 */
+	 var originalHtmlMethod = $.fn.html;
+	 
+	 $.fn.html = function (htmlContent){
+		var toReturn = originalHtmlMethod(htmlContent);
+		this.translate();
+		this.find("[data-translate]").each(function(index, elt){ $(elt).translate(); });
+		return toReturn;
+	 }
+	
+	/**
+	 * Try to translate the inner text, or the attribute if needed
+	 */
+	$.fn.translate = function() {
+	    if(!$.translate || !$.translate.properties){
+			// The plugin is not really available ...
+	        return;
+	    }
+	    
+        var jSelf = $(this), dataTranslate = jSelf.attr("data-translate");
+        if(dataTranslate){
+            $.each(dataTranslate.split(";"), function(index, translate){
+                var index = translate.indexOf(":");
+            
+                if(index > 0){ // We need an attribute name !
+                    jSelf.attr(translate.substring(0, index), $.translate.properties[translate.substring(index + 1)]);
+                    
+                } else {
+                    jSelf.text($.translate.properties[translate]);
+                }
+            });
+            
+            jSelf.removeAttr("data-translate"); // Shall we keep it ? In the case that we change the globalization on the fly ?
+        }
+    };
+	
+	/**
+	 * When the DOM is ready, we try to translate the elements
+	 */
+	 $(document).ready(function(){
+		if(!$.translate || !$.translate.properties){
+			// The plugin is not really available ...
+	        return;
+	    }
+		
+		// Now, we try to load the default globalization language. We will check first which one we will use.
+        $("head:first").append($("<script />",  { "src": $.translate.pathLanguage($.translate.defaultLanguage) }));
+	 
+		// We try to translate the elements
+        $("[data-translate]").each(function (index, elt) { $(elt).translate(); });
+		
+		// Each time a node is inserted, we try to translate it !     
+		$(document).bind("DOMNodeInserted", function(event){
+			var jElement = $(event.srcElement ? event.srcElement : event.target);
+			jElement.translate();
+			jElement.find("[data-translate]").each(function(index, elt){ $(elt).translate(); });
+		});
+    });
+})(jQuery);
