a7ffbd8b07e1191f7697de3a5198f82421fba5a4
Julien Roche <jroche@objetdirect.com>
2013-05-15 17:39:19 +0200

    Enhancements for the jquery.serializeObject plugin
    - missing "use strict"
    - indentation
    - better dection of jQuery for nodeJs

--- jQuery/serializeObject/jquery.serializeObject.js
+++ jQuery/serializeObject/jquery.serializeObject.js
@@ -8,27 +8,34 @@
  */
 
 (function(){
+	"use strict";
+
 	function definition($){
 		$.fn.serializeObject = function () {
             var o = {};
             var a = this.serializeArray();
+
             $.each(a, function () {
                 if (o[this.name] !== undefined) {
                     if (!o[this.name].push) {
                         o[this.name] = [o[this.name]];
                     }
+
                     o[this.name].push(this.value || "");
+
                 } else {
                     o[this.name] = this.value || "";
                 }
             });
+
             return o;
         };
 	}
 
 	if (typeof module === "object" && typeof module.exports === "object") {
 		// Node approach
-		definition(module.require("jquery"));
+		var jqInstance = window.jQuery ? window.jQuery : module.require("jquery");
+		jqInstance && definition(jqInstance);
 
 	} else if (typeof define === "function" && define.amd) {
 		// AMD approach
